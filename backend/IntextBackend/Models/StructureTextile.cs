1fb1f3afcddf7e73e987ab18f63c1c2c27fe05f6
YuiZoldyck <betasan28@gmail.com>
2023-04-10 16:04:00 -0600

    scaffold database

---
+++ backend/IntextBackend/Models/StructureTextile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntextBackend.Models
+{
+    public partial class StructureTextile
+    {
+        public long MainStructureid { get; set; }
+        public long MainTextileid { get; set; }
+    }
+}
