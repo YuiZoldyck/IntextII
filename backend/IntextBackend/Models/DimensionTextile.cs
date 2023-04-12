6ac43b58ff4cca33fc44568519d57d97afb9a64a
autdel32 <autdel@byu.edu>
2023-04-12 00:29:01 -0600

    New Models!!!

---
+++ backend/IntextBackend/Models/DimensionTextile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntextBackend.Models
+{
+    public partial class DimensionTextile
+    {
+        public long MainDimensionid { get; set; }
+        public long MainTextileid { get; set; }
+    }
+}
