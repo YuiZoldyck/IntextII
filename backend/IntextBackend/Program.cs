679c9db5e8a332323013e29980df36091e70a3ee
autdel32 <autdel@byu.edu>
2023-04-12 00:21:38 -0600

    changes

--- backend/IntextBackend/Program.cs
+++ backend/IntextBackend/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using IntextBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BurialDataContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("BurialDataDb"))
+);
 
 var app = builder.Build();
 
